ecf69f823e2a4b2972aa9051edf2d809f8150409
JacobJohansson <jacobknuts@gmail.com>
2026-02-20 05:46:27 +0100

    Added tranistion animation to button

--- 143/src/components/Job.jsx
+++ 143/src/components/Job.jsx
@@ -30,7 +30,7 @@ const Job = ({ job }) => {
       {/* Knapp */}
       <a
         href={`#job-${job.id}`}
-        className="inline-block bg-blue-600 text-white px-4 py-2 rounded-md font-medium hover:bg-blue-700 transition-colors"
+        className="inline-block bg-blue-600 text-white px-4 py-2 rounded-md font-medium transition delay-150 duration-300 ease-in-out hover:bg-blue-700 hover:-translate-y-1 hover:scale-110"
       >
         Ansök nu
       </a>
