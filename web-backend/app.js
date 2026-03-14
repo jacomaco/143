6ba5176642c22459a9ce4b9b340b3493f7c9d090
Jacob Johansson <jacobknuts@gmail.com>
2026-03-14 23:47:37 +0100

    Started implementing rate-limiting, the site does refuse requests, need to set higher rate limit so that site is usable

--- web-backend/app.js
+++ web-backend/app.js
@@ -6,8 +6,11 @@ const middleware = require('./utils/middleware');
 const jobsRouter = require('./controllers/jobs');
 const adminRouter = require('./controllers/admin');
 const messagesRouter = require('./controllers/messages');
+const rateLimit = require('express-rate-limit');
 
 const app = express()
+
+app.set('trust proxy', 1); // (Viktigt för Render, annars blockerar den fel IP)
 
 logger.info('connection to', config.MONGODB_URI)
 
@@ -23,6 +26,16 @@ mongoose
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
+
+// För testning: Max 3 anrop per minut
+const testLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minut
+    max: 3,
+    message: { error: 'Test av Rate Limit: Du har blivit blockerad för att du klickade för snabbt!' }
+});
+
+// Använd limitern på alla rutter som börjar med /api/
+app.use('/api/', testLimiter);
 
 app.use('/api/jobs', jobsRouter)
 app.use('/api/admin', adminRouter)
